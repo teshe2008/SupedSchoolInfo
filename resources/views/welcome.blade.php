3939a17459bd8d6e36797f89713e7b359db4aa82
teshome hailu <teshe@2008@hotmail.com>
2021-01-28 10:36:41 +0300

    authentication routes added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,8 @@
 @extends('layouts.app')
-<div class="container"></div>
+<div class="container-fluid">
+<div class="jumbotron text-center bg-success">
+  <h1 class="">SupEdSIS</h1>
+  <p>Support Education School information management</p>
+  <div><a href="{{url('auth.login')}}"><button class="btn btn-primary">login</button></a></div>
+</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/login',function(){
     return view('auth.login');
 });
+Route::get('/register',function(){
+return view('auth.register');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
