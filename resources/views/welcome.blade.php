43977824db5beaa263afa802644bbe1f9fe82fff
teshome hailu <teshe@2008@hotmail.com>
2021-01-28 12:18:51 +0300

    logout added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
     <div class="jumbotron text-center bg-success">
         <h1 class="">SupEdSIS</h1>
         <p>Support Education School information management</p>
-        <div><a href="{{url('auth.login')}}">
+        <div><a href="{{route('login')}}">
         <button class="btn btn-primary">login</button></a>
-        <a href="{{url('auth.register')}}">
+        <a href="{{route('register')}}">
         <button class="btn btn-secondary">Register</button></a>
         </div>
     </div>
