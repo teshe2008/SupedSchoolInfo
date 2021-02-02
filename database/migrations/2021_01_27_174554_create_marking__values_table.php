4a52a07193b0cd5ec4b2cad400f191dcdfc34594
teshome hailu <teshe@2008@hotmail.com>
2021-02-02 07:34:07 +0300

    migration added

--- database/migrations/2021_01_27_174554_create_marking__values_table.php
+++ database/migrations/2021_01_27_174554_create_marking__values_table.php
@@ -15,6 +15,14 @@ class CreateMarkingValuesTable extends Migration
     {
         Schema::create('marking__values', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('marking_point_id');
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('teacher_id');
+            $table->string('value');
+            $table->date('date');
+            $table->foreign('marking_point_id')->references('id')->on('marking_points');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('teacher_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
