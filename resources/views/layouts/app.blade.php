46ea2824c54512b60f9135691cfaea6fd2f5a098
teshome hailu <teshe@2008@hotmail.com>
2021-02-03 14:13:36 +0300

    spatie middleware added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,22 +121,6 @@
         @include('layouts.sidebar')
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Dashboard </h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v2</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
             <main>
                 @yield('content')
             </main>
