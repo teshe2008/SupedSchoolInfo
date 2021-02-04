6000a3b33c1ea4b6214dd165aa9c8deb15734dc6
henok2424 <henimarly8@gmail.com>
2021-02-04 03:22:05 -0500

    update users form looking

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,16 +2,23 @@
 
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create New User</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
-        </div>
+<section class="content-header">
+<div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Create New User</h1>
+      </div>
+      <div class="col-sm-6">
+        <a class="btn btn-primary float-right" href="{{ route('users.index') }}"> Back</a>
+
+          {{-- <a class="btn btn-success float-right" href="{{ route('users.create') }}"> Create New User</a> --}}
+
+      </div>
+
     </div>
-</div>
+  </div><!-- /.container-fluid -->
+</section>
+
 
 
 @if (count($errors) > 0)
@@ -24,47 +31,41 @@
     </ul>
   </div>
 @endif
-
-
-
 {!! Form::open(array('route' => 'users.store','method'=>'POST')) !!}
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Email:</strong>
-            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Password:</strong>
-            {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Confirm Password:</strong>
-            {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Role:</strong>
-            {!! Form::select('roles[]', $roles,[], array('class' => 'form-control','multiple')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
+<div class="card card-primary">
+    <!--   <!-- general form elements -->
+      <div class="card card-primary">
+        <form role="form">
+          <div class="card-body">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name</label>
+              {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+              </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Confirm Password</label>
+                {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
+              </div>
+              <div class="form-group">
+                <label>Role</label>
+                {!! Form::select('roles[]', $roles,[], array('class' => 'form-control','multiple')) !!}
+              </div>
+            </div>
+          <!-- /.card-body -->
+
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+        {!! Form::close() !!}
+      </div>
 </div>
-{!! Form::close() !!}
-
-
 
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,27 +3,96 @@
 
 
 @section('content')
+<section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Users Management</h1>
+        </div>
+        <div class="col-sm-6">
 
-<div class="row">
-
-    <div class="col-lg-12 margin-tb">
-
-        <div class="pull-left">
-
-            <h2>Users Management</h2>
+            <a class="btn btn-success float-right" href="{{ route('users.create') }}"> Create New User</a>
 
         </div>
 
-        <div class="pull-right">
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
 
-            <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
 
-        </div>
-
+<div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Users List</h3>
     </div>
 
-</div>
+    <!-- /.card-header -->
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th style="width: 10px">#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Rolles</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $key => $user)
 
+            <tr>
+
+              <td>{{ ++$i }}</td>
+
+              <td>{{ $user->name }}</td>
+
+              <td>{{ $user->email }}</td>
+
+              <td>
+
+                @if(!empty($user->getRoleNames()))
+
+                  @foreach($user->getRoleNames() as $v)
+
+                     <label class="badge badge-success">{{ $v }}</label>
+
+                  @endforeach
+
+                @endif
+
+              </td>
+
+              <td>
+
+                 <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
+
+                 <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+
+                  {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+
+                      {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+
+                  {!! Form::close() !!}
+
+              </td>
+
+            </tr>
+
+           @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!-- /.card-body -->
+    {{-- <div class="card-footer clearfix">
+      <ul class="pagination pagination-sm m-0 float-right">
+        <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+      </ul>
+    </div> --}}
+  </div>
 
 
 @if ($message = Session::get('success'))
@@ -35,69 +104,6 @@
 </div>
 
 @endif
-
-
-
-<table class="table table-bordered">
-
- <tr>
-
-   <th>No</th>
-
-   <th>Name</th>
-
-   <th>Email</th>
-
-   <th>Roles</th>
-
-   <th width="280px">Action</th>
-
- </tr>
-
- @foreach ($data as $key => $user)
-
-  <tr>
-
-    <td>{{ ++$i }}</td>
-
-    <td>{{ $user->name }}</td>
-
-    <td>{{ $user->email }}</td>
-
-    <td>
-
-      @if(!empty($user->getRoleNames()))
-
-        @foreach($user->getRoleNames() as $v)
-
-           <label class="badge badge-success">{{ $v }}</label>
-
-        @endforeach
-
-      @endif
-
-    </td>
-
-    <td>
-
-       <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
-
-       <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-
-        {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-
-            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-
-        {!! Form::close() !!}
-
-    </td>
-
-  </tr>
-
- @endforeach
-
-</table>
-
 
 
 {!! $data->render() !!}
